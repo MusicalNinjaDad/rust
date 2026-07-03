14bec0e45475a0cb9e9cd95f42e1846f4294ddd5
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-03 08:33:15 +0000

    update build test fixture

--- ninja-build_rs/examples/assert_matches/build.rs
+++ ninja-build_rs/examples/assert_matches/build.rs
@@ -1,12 +1,9 @@
 use autocfg::AutoCfg;
-use ninja_build_rs::{
-    Result,
-    nightly::{Nightly, cargo_allowed_features},
-};
+use ninja_build_rs::prelude::*;
 
 fn main() -> Result<()> {
     let ac = AutoCfg::new()?;
     let allowed_features = cargo_allowed_features()?;
-    ac.emit_unstable_feature("assert_matches", &allowed_features);
+    ac.emit_unstable_feature(assert_matches, &allowed_features);
     Ok(())
 }
