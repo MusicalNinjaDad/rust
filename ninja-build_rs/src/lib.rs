500f13b0f78c615f183a6c4e07fd34a86d5269d3
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-03 15:17:42 +0000

    add usage example

--- ninja-build_rs/src/lib.rs
+++ ninja-build_rs/src/lib.rs
@@ -1,7 +1,44 @@
 //! Designed to help create good build scripts, with a focus on ease of use for you,
 //! valuable output in `cargo build -vv` & no annoying surprises for anyone downstream.
 //!
-//! ## Prelude
+//! # Usage
+//!
+//! ```rust, no_run
+//! # use std::collections::HashSet;
+//! use ninja_build_rs::prelude::*;
+//!
+//! // Result uses BuildError to give meaningful messages
+//! fn main() -> Result<()> {
+//!
+//!     // get an environment variable and re-run build script if it changes.
+//!     let my_var: String = get_var("MY_VAR")?;
+//!
+//!     // get values from an environment variable, separated by the
+//!     // OS path separator and re-run build script if it changes.
+//!     let my_vals: HashSet<String> = split_var("MY_VALUES")?;
+//!     if my_vals.contains("some_value") {
+//!         unimplemented!("do something")
+//!     }
+//!
+//!     // get a new AutoCfg or provide a valuable error
+//!     // rather than panicing.
+//!     let ac = AutoCfg::new()?;
+//!
+//!     // check to see if the downstream crate has defined
+//!     // `unstable.allow-features` in `.cargo/config.toml`.
+//!     // It is mandatory to perform this check and pass the
+//!     // result to any calls to `emit_unstable_feature`
+//!     let allowed_features = cargo_allowed_features()?;
+//!
+//!     // We want to make use of `assert_matches` if it is available
+//!     ac.emit_unstable_feature(assert_matches, &allowed_features);
+//!     //                       ^^^^^^^^^^^^^^ - enum variant to avoid typos
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! # Prelude
 //!
 //! ```rust
 //! use ninja_build_rs::prelude::*;
@@ -17,6 +54,8 @@
 //!   enum [`UnstableFeature`](nightly::UnstableFeature) to provide a safe way to identify the
 //!   availability of nightly features & handle the future stabilisation process without additional
 //!   effort on your part. All while respecting any `allow-feature` whitelists.
+//!
+
 use std::{collections::HashSet, env::VarError, ffi::OsString};
 
 /// Recommended prelude: `use ninja-build_rs::prelude::*`
