e243861e575d64eea722bd278f9ca7c0491b19d1
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-03 08:31:27 +0000

    update xtask build script to use UnstableFeature

--- ninja-xtask/build.rs
+++ ninja-xtask/build.rs
@@ -4,9 +4,9 @@ fn main() -> Result<()> {
     let allowed_features = cargo_allowed_features()?;
     let ac = autocfg::new();
 
-    ac.emit_unstable_feature("never_type", &allowed_features);
-    ac.emit_unstable_feature("try_trait_v2", &allowed_features);
-    ac.emit_unstable_feature("try_trait_v2_residual", &allowed_features);
+    ac.emit_unstable_feature(never_type, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
 
     Ok(())
 }
