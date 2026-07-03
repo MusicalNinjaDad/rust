bf19ac6ad8bbde13c322656a6256d260418e8f07
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-03 18:58:58 +0000

    use correct msrv when testing fixture

--- ninja-build_rs/examples/assert_matches/test.sh
+++ ninja-build_rs/examples/assert_matches/test.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 RUSTC_BOOTSTRAP=0 cargo +stable test
 cargo +nightly test
 cargo +nightly-2026-01-01 test
-RUSTC_BOOTSTRAP=0 cargo +1.85.0 test
+RUSTC_BOOTSTRAP=0 cargo +1.85.1 test
