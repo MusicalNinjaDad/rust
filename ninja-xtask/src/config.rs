19f50f21f8c6a1c4e72679d19d81d8b8bae5f9aa
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-26 14:09:09 +0000

    test Config::vars

---
+++ ninja-xtask/src/config.rs
@@ -0,0 +1,64 @@
+//! Load custom config from `[workspace.metadata.ninja-xtask]` in `Cargo.toml`
+
+use serde_json::{Map, Value};
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
+pub struct Config {
+    env: Map<String, Value>,
+}
+
+impl From<Value> for Config {
+    fn from(metadata: Value) -> Self {
+        let our_config = metadata
+            .as_object()
+            .and_then(|metadata| metadata.get("ninja-xtask"));
+        match our_config {
+            Some(config) => Self {
+                env: config
+                    .as_object()
+                    .and_then(|config| config.get("env"))
+                    .and_then(|env| env.as_object())
+                    .cloned()
+                    .unwrap_or_default(),
+            },
+            None => Self::default(),
+        }
+    }
+}
+
+impl Config {
+    pub fn vars(&self) -> impl IntoIterator<Item = (&String, &str)> {
+        self.env
+            .iter()
+            .filter_map(|(key, value)| value.as_str().map(|value| (key, value)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::OsStr;
+
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn set_var() {
+        let metadata = json!({
+            "ninja-xtask": {
+                "env": {
+                    "LD_LIBRARY_PATH": false,
+                    "CARGO_INCREMENTAL": "1"
+                }
+            }
+        });
+        let config = Config::from(metadata);
+        let vars: Vec<_> = config.vars().into_iter().collect();
+        let expected = [(&"CARGO_INCREMENTAL".to_string(), "1")];
+        assert_eq!(vars, expected);
+        let _assert_os_strs: Vec<(&OsStr, &OsStr)> = vars
+            .into_iter()
+            .map(|(k, v)| (k.as_ref(), v.as_ref()))
+            .collect();
+    }
+}
