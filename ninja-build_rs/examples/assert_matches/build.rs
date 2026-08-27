5c1158b4b66f0d3df140041d589d7aed6479c5bc
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-27 04:56:46 +0000

    rm ninja-build_rs

--- ninja-build_rs/examples/assert_matches/build.rs
+++
@@ -1,9 +0,0 @@
-use autocfg::AutoCfg;
-use ninja_build_rs::prelude::*;
-
-fn main() -> Result<()> {
-    let ac = AutoCfg::new()?;
-    let allowed_features = cargo_allowed_features()?;
-    ac.emit_unstable_feature(assert_matches, &allowed_features);
-    Ok(())
-}
--- ninja-build_rs/examples/assert_matches/test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -euxo pipefail
-
-RUSTC_BOOTSTRAP=0 cargo +stable test
-cargo +nightly test
-cargo +nightly-2026-01-01 test
-RUSTC_BOOTSTRAP=0 cargo +1.85.1 test
